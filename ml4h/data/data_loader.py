78962d9ffe214e1b3404a711f8edd805ce17404e
ndiamant <ndiamant@broadinstitute.org>
2021-01-11 16:39:14 -0500

    added numpy_collate_fn allowing tensorflow use of dataloader

--- ml4h/data/data_loader.py
+++ ml4h/data/data_loader.py
@@ -1,5 +1,6 @@
-from typing import Callable, Dict, List
+from typing import List
 
+import numpy as np
 from torch.utils.data import Dataset
 
 from ml4h.data.defines import Batch, SampleGetter, SampleID
@@ -24,4 +25,18 @@ class ML4HDataset(Dataset):
         return self.sample_getter(sample_id)
 
 
-# TODO: training sample getter, validation, test
+def numpy_collate_fn(batches: List[Batch]) -> Batch:
+    """
+    Merges a list of ml4h batch formatted data.
+    Can be used as 'collate_fn` in torch.utils.data.DataLoader
+    so that the torch data loader is compatible with tensorflow models
+    """
+    in_batch = {
+        k: np.stack([sample[0][k] for sample in batches]).astype(np.float32)
+        for k in batches[0][0]
+    }
+    out_batch = {
+        k: np.stack([sample[1][k] for sample in batches]).astype(np.float32)
+        for k in batches[0][1]
+    }
+    return in_batch, out_batch
