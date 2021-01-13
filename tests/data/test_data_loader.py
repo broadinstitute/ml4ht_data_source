a565e97c3fca279bc0f2a36081d7f8b22faedb54
ndiamant <ndiamant@broadinstitute.org>
2021-01-13 17:30:05 -0500

    added a functional way to zip together different modalities

--- tests/data/test_data_loader.py
+++ tests/data/test_data_loader.py
@@ -1,7 +1,13 @@
+import pytest
 import numpy as np
 from torch.utils.data import DataLoader
 
-from ml4ht.data.data_loader import ML4HDataset, numpy_collate_fn
+from ml4ht.data.data_loader import (
+    SampleGetterDataset,
+    numpy_collate_fn,
+    CallbackDataset,
+    ML4HCallbackDataset,
+)
 from ml4ht.data.defines import Batch
 
 
@@ -9,14 +15,14 @@ def sample_getter(sample_id: int) -> Batch:
     return {"in": np.full((3, 3), sample_id)}, {"out": np.full((1,), sample_id)}
 
 
-dataset = ML4HDataset(list(range(100)), sample_getter)
+sample_getter_dataset = SampleGetterDataset(list(range(100)), sample_getter)
 
 
 def test_numpy_data_loader():
     """does numpy_collate_fn correctly stack batch data"""
     batch_size = 5
     data_loader = DataLoader(
-        dataset,
+        sample_getter_dataset,
         batch_size=batch_size,
         collate_fn=numpy_collate_fn,
     )
@@ -27,3 +33,61 @@ def test_numpy_data_loader():
                 np.testing.assert_allclose(actual_in[k][j], v)
             for k, v in expected_out.items():
                 np.testing.assert_allclose(actual_out[k][j], v)
+
+
+def simple_callback(arg):
+    return float(arg)
+
+
+def simple_transform(tensor):
+    return -tensor
+
+
+def test_callback_dataset():
+    dataset = CallbackDataset(
+        "floatify",
+        callback_args=list(map(str, range(100))),
+        callback=simple_callback,
+        transforms=[simple_transform],
+    )
+    assert len(dataset) == 100
+    for i, out in enumerate(dataset):
+        assert i == -out
+
+
+def test_ml4h_callback_dataset():
+    inp_name = "floatify_in"
+    dataset_in = CallbackDataset(
+        inp_name,
+        callback_args=list(map(str, range(100))),
+        callback=simple_callback,
+        transforms=[simple_transform],
+    )
+    out_name = "floatify_out"
+    dataset_out = CallbackDataset(
+        out_name,
+        callback_args=list(map(str, range(100))),
+        callback=simple_callback,
+    )
+    merged = ML4HCallbackDataset([dataset_in], [dataset_out])
+    assert len(merged) == 100
+    for i, (inp, out) in enumerate(merged):
+        assert inp[inp_name] == -i
+        assert out[out_name] == i
+
+
+def test_ml4h_callback_dataset_bad_merge():
+    dataset1 = CallbackDataset(
+        "floatify",
+        callback_args=list(map(str, range(100))),
+        callback=simple_callback,
+        transforms=[simple_transform],
+    )
+    dataset2 = CallbackDataset(
+        "floatify",
+        callback_args=list(map(str, range(101))),
+        callback=simple_callback,
+        transforms=[simple_transform],
+    )
+    with pytest.raises(ValueError):
+        ML4HCallbackDataset([dataset1], [dataset2])
