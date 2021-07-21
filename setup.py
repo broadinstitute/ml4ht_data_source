33ff91efb945b77ad3bb6f274c90bb492725fa4f
ndiamant <ndiamant@broadinstitute.org>
2021-07-21 16:29:52 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ml4ht",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=["numpy", "pandas"],
