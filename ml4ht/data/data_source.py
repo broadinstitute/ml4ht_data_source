81d4496c02a88503cd4433dc36dbaae19400af76
ndiamant <ndiamant@broadinstitute.org>
2021-08-18 18:04:19 -0400

    Add normal pytorch Dataset for DataSources

--- ml4ht/data/data_source.py
+++ ml4ht/data/data_source.py
@@ -10,7 +10,7 @@ from typing import (
     Mapping,
 )
 
-from torch.utils.data import IterableDataset, get_worker_info
+from torch.utils.data import IterableDataset, Dataset, get_worker_info
 import numpy as np
 from ml4ht.data.defines import EXCEPTIONS
 
@@ -71,7 +71,58 @@ def sample_id_epoch_generator(
         yield [{sample_id_name: sample_ids[idx]} for idx in indices]
 
 
-class TrainingDataset(IterableDataset):
+class TrainingDataset(Dataset):
+    """
+    Indexable of training data from `DataSources` for use in pytorch `DataLoader`.
+    To be able to skip errors during data loading, use `TrainingIterableDataset` below.
+    """
+
+    def __init__(
+        self,
+        data_sources: List[DataSource],
+        data_indices: Sequence[DataIndex],
+    ):
+        """
+        :param data_sources: Sources of data for inputs and outputs to model
+        :param data_indices: Indices for each epoch of data
+        """
+        super(TrainingIterableDataset).__init__()
+        self.data_sources = data_sources
+        self.data_indices = data_indices
+
+    def __len__(self):
+        return len(self.data_indices)
+
+    def training_example(self, data_idx: DataIndex) -> Tuple[Data, Data]:
+        combined_inputs, combined_outputs = {}, {}
+        for data_source in self.data_sources:
+            data_in, data_out = data_source(data_idx)
+            for name, modality in data_in.items():
+                if name in combined_inputs:
+                    raise ValueError(
+                        f"Multiple DataSources share the same input modality called {name}",
+                    )
+                combined_inputs[name] = modality
+            for name, modality in data_out.items():
+                if name in combined_outputs:
+                    raise ValueError(
+                        f"Multiple DataSources share the same output modality called {name}",
+                    )
+                combined_outputs[name] = modality
+        return combined_inputs, combined_outputs
+
+    def __getitem__(self, item: int) -> Tuple[Data, Data]:
+        data_idx = self.data_indices[item]
+        return self.training_example(data_idx)
+
+
+class TrainingIterableDataset(IterableDataset):
+    """
+    Iterable over epochs of training data from `DataSources` for use in pytorch `DataLoader`.
+    More flexible version of `TrainingDataset` that can skip errors,
+    but requires defining an Iterator over epochs of `DataIndex`s.
+    """
+
     # keys in error list
     error_key = "error"
     source_key = "source"
@@ -86,13 +137,13 @@ class TrainingDataset(IterableDataset):
     ):
         """
 
-        :param data_sources: Get data for inputs and outputs to model
+        :param data_sources: Sources of data for inputs and outputs to model
         :param epoch_indices_iterator: Iterator over epochs of `DataIndex`s
             For example, see `sample_id_epoch_indices` above.
         :param raise_errors: Whether to raise or skip errors during calling of
             `DataFetcher`s
         """
-        super(TrainingDataset).__init__()
+        super(TrainingIterableDataset).__init__()
         self.data_sources = data_sources
         self.epoch_indices_iterator = epoch_indices_iterator
         self.raise_errors = raise_errors
