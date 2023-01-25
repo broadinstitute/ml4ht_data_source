871d4c0c949b145eed80022e027b602ff13220b5
Samuel Friedman <lucidtronix@gmail.com>
2023-01-25 10:23:10 -0500

    bump version

--- ml4ht/data/util/data_frame_data_description.py
+++ ml4ht/data/util/data_frame_data_description.py
@@ -12,28 +12,34 @@ class DataFrameDataDescription(DataDescription):
         self,
         df: pd.DataFrame,
         col: str,
-        process_col: Callable[[Any], Tensor] = None,
         name: str = None,
+        loading_option_name: str = None,
+        value_to_tensor: Callable[[Any], Tensor] = None,
     ):
         """
-        Gets data from a column of the provided DataFrame.
-        :param df: Must be multi-indexed with sample_id, loading_option
-        # TODO: allow multiple loading options
+        DataDescription which gets data from a specific column of the provided DataFrame.
+
+        :param df: Must be indexed with sample_id and optionally with another loading_option
         :param col: The column name to get data from
-        :param process_col: Function to turn the column value into Tensor
+        :param name: The DataDescription name if different from the column name to get data from
+        :param loading_option_name: Optional additional index column besides sample_id if needed to load data
+        :param value_to_tensor: Optional Function to transform the column value into a Tensor
         """
-        self.process_col = process_col or self._default_process_call
-        self.df = df.sort_index()[col]
-        self.option_name = self.df.index.names[1]
         self._name = name or col
+        self.df = df.sort_index()[col]
+        self.loading_option_name = loading_option_name
+        self.value_to_tensor = value_to_tensor or self._default_value_to_tensor
 
     @staticmethod
-    def _default_process_call(x: Any) -> Tensor:
+    def _default_value_to_tensor(x: Any) -> Tensor:
         return np.array(x)
 
     def get_loading_options(self, sample_id: SampleID) -> List[LoadingOption]:
-        options = self.df.loc[sample_id].index
-        return [{self.option_name: option} for option in options]
+        if self.loading_option_name is None:
+            return [{}]
+        else:
+            options = self.df.loc[sample_id].index
+            return [{self.loading_option_name: option} for option in options]
 
     @property
     def name(self) -> str:
@@ -44,5 +50,8 @@ class DataFrameDataDescription(DataDescription):
         sample_id: SampleID,
         loading_option: LoadingOption,
     ) -> Tensor:
-        col_val = self.df.loc[sample_id, loading_option[self.option_name]]
-        return self.process_col(col_val)
+        if self.loading_option_name is None:
+            col_val = self.df.loc[sample_id]
+        else:
+            col_val = self.df.loc[sample_id, loading_option[self.loading_option_name]]
+        return self.value_to_tensor(col_val)
--- tests/data/util/test_data_frame_data_description.py
+++ tests/data/util/test_data_frame_data_description.py
@@ -5,6 +5,13 @@ from ml4ht.data.util.data_frame_data_description import DataFrameDataDescription
 
 TEST_DF = pd.DataFrame(
     {
+        "sample_id": [0, 1, 2],
+        "value": [1.1, 1.2, 10.0],
+    },
+).set_index(["sample_id"])
+
+TEST_DF_OPTION = pd.DataFrame(
+    {
         "sample_id": [0, 0, 1, 2],
         "option": ["a", "b", "a", "c"],
         "value": [1.1, 1.2, -0.2, 10.0],
@@ -14,8 +21,9 @@ TEST_DF = pd.DataFrame(
 
 def test_get_loading_options():
     dd = DataFrameDataDescription(
-        TEST_DF,
+        TEST_DF_OPTION,
         "value",
+        loading_option_name="option",
     )
     for sample_id in TEST_DF.index.levels[0]:
         options = dd.get_loading_options(sample_id)
@@ -25,17 +33,28 @@ def test_get_loading_options():
             assert any(expected_option == option["option"] for option in options)
 
 
-def test_get_raw_data():
+def test_get_raw_data_option():
     dd = DataFrameDataDescription(
-        TEST_DF,
+        TEST_DF_OPTION,
         "value",
-        lambda x: -x,
+        value_to_tensor=lambda x: -x,
     )
     for sample_id in TEST_DF.index.levels[0]:
         options = dd.get_loading_options(sample_id)
         for option in options:
             val = dd.get_raw_data(sample_id, option)
             assert val == -TEST_DF.loc[sample_id, option["option"]][0]
+
+
+def test_get_raw_data():
+    dd = DataFrameDataDescription(
+        TEST_DF,
+        "value",
+        value_to_tensor=lambda x: -x,
+    )
+    for sample_id in TEST_DF.index.levels[0]:
+        val = dd.get_raw_data(sample_id, None)
+        assert val == -TEST_DF.loc[sample_id][0]
 
 
 class TestName:
